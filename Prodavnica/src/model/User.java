9b17ae4a469112ccfedf9d4ec5fbcbaaca20498b
Nikola <Nikola@DESKTOP-NUBL7DA>
2019-04-15 20:03:41 +0200

    Delom sredjeno

---
+++ Prodavnica/src/model/User.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idUser;
+	private String userName;
+	private String password;
+	private double blance;
+	
+	private List<Product> listOfProducts = new ArrayList<Product>();
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public double getBlance() {
+		return blance;
+	}
+
+	public void setBlance(double blance) {
+		this.blance = blance;
+	}
+
+	public List<Product> getListOfProducts() {
+		return listOfProducts;
+	}
+
+	public void setListOfProducts(List<Product> listOfProducts) {
+		this.listOfProducts = listOfProducts;
+	}
+	
+
+}
